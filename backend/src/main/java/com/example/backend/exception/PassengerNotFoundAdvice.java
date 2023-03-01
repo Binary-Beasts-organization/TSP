6c5f7ff0488e3fa81a1187ef5888c689780374ba
ashan-tharaka <ashantharaka715@gmail.com>
2023-03-01 15:33:17 +0530

    update passenger class

--- backend/src/main/java/com/example/backend/exception/PassengerNotFoundAdvice.java
+++ backend/src/main/java/com/example/backend/exception/PassengerNotFoundAdvice.java
@@ -1,4 +1,23 @@
 package com.example.backend.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class PassengerNotFoundAdvice {
+    @ResponseBody
+    @ExceptionHandler(PassengerNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+
+    public Map<String,String> exceptionHandler(UserPrincipalNotFoundException exception){
+        Map<String,String> errorMap=new HashMap<>();
+        errorMap.put("errorMesssager",exception.getMessage());
+        return errorMap;
+
+    }
 }
